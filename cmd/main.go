d38339f15f7fb1774ee666ec3daa84f9213b4091
khalikov <aliev-ali-200201@mail.ru>
2023-08-30 21:23:49 +0300

    create service and repository interfaces and structures

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"log"
-	todo "github.com/khlkvmgmd/to-do-app"
+
+	"github.com/khlkvmgmd/to-do-app"
 	"github.com/khlkvmgmd/to-do-app/pkg/handler"
+	"github.com/khlkvmgmd/to-do-app/pkg/repository"
+	"github.com/khlkvmgmd/to-do-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRotes()); err != nil{
 		log.Fatalf("error: %s", err.Error())

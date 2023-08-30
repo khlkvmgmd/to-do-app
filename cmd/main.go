8ce53e022c9565e46aa5a1b3fc0a36829cecc07a
khalikov <aliev-ali-200201@mail.ru>
2023-08-30 21:46:54 +0300

    configuration files (lib: spf13/viper)

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,26 @@ import (
 	"github.com/khlkvmgmd/to-do-app/pkg/handler"
 	"github.com/khlkvmgmd/to-do-app/pkg/repository"
 	"github.com/khlkvmgmd/to-do-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil{
+		log.Fatalf("error initialazing config %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRotes()); err != nil{
-		log.Fatalf("error: %s", err.Error())
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRotes()); err != nil{
+		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }

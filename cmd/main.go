05a78b6830ac38a5e989116dec607ce2fc395b3b
khalikov <aliev-ali-200201@mail.ru>
2023-08-30 20:55:46 +0300

    create antpoints

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"log"
-
 	todo "github.com/khlkvmgmd/to-do-app"
+	"github.com/khlkvmgmd/to-do-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil{
+	if err := srv.Run("8000", handlers.InitRotes()); err != nil{
 		log.Fatalf("error: %s", err.Error())
 	}
 }

5ebb56faf9a534b4acb4decb9cb05ef0c8e96a26
khalikov <aliev-ali-200201@mail.ru>
2023-09-05 15:14:19 +0300

    connect to db

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,41 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
 	"log"
 
-	"github.com/khlkvmgmd/to-do-app"
+	todo "github.com/khlkvmgmd/to-do-app"
 	"github.com/khlkvmgmd/to-do-app/pkg/handler"
 	"github.com/khlkvmgmd/to-do-app/pkg/repository"
 	"github.com/khlkvmgmd/to-do-app/pkg/service"
+	
 	"github.com/spf13/viper"
 )
 
 func main() {
-	if err := initConfig(); err != nil{
-		log.Fatalf("error initialazing config %s", err.Error())
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5436",
+		Username: "postgres",
+		Password: "qwerty",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
-
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("8000"), handlers.InitRotes()); err != nil{
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRotes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
